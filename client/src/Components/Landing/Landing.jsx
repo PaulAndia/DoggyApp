59b354bc4b442005d11b1aa258e1db9ead7b6890
PaulAndia <paulandresandia@gmail.com>
2022-09-28 17:24:38 -0400

    land

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import styles from "./Landing.module.css"
-import land from "../Images/l3.jpg"
+import land from "../Images/L12.jpg"
 
 export function Landing() {
     return (
         <div className={styles.cover}>
-                <img src={land} alt="landing" width="100%"/>
+                <img src={land} alt="landing"/>
             <Link to="/home">
-                <button>
+                <div className={styles.wrap}>
+                <button className={styles.button}>
                     START
                 </button>
+                </div>
+                
             </Link>
         </div>
     )
